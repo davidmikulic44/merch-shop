3e5250e4b3fc34f4168d042cf850b9e321a9edf6
davidmikulic44 <david.mikulic44@gmail.com>
2023-12-06 14:28:43 +0100

    seperating php files

---
+++ src/php/footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="footer-links">
+        <a href="https://instagram.com/sbl022">
+            <img src="assets/images/instagram.png" class="image-link" alt="instagram link">
+        </a>
+        <a href="https://www.youtube.com/@SBLici">
+            <img src="assets/images/youtube.png" class="image-link" alt="youtube link">
+        </a>
+    </div>
+
+    <h1 class="footer-text">SBL ® 2023</h1>
+
+    <div class="spotify-link">
+        <img class="spotify-image" src="assets/images/spotify.jpg" alt="spotify image">
+        <div class="spotify-text">
+            <h1 class="spotify-title">Loading...</h1>
+            <h1 class="spotify-description"></h1>
+        </div>
+    </div>
+
+</footer>
